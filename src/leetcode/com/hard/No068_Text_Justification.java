b2e44a7203d46f436e486fc1e87c17c653b4d584
Jian Zhang <jianzhang@tcl.com>
2016-03-09 09:14:25 -0800

    finish the coding of text justification

--- src/leetcode/com/hard/No068_Text_Justification.java
+++ src/leetcode/com/hard/No068_Text_Justification.java
@@ -30,14 +30,23 @@ import java.util.List;
  * "justification.  "
  * ]
  * ************************************************
- * Solution:
+ * Solution: 终于看明白啦
+ * 基本概念：单词间的space slot 该是由当前元素下标 减去最后一个待处理元素下标 再减一来获得（因为当前元素属于下一行的首元素），
+ * 但是本行最后一个元素后面不需要space
+ * 那么每个space slot放多少space？可以通过maxWidth-当前字符串总长获得，有余数的话，余数被分别放入前几个元素（j-lastI<extra）
+ * 先对字符串数组进行循环,(i == wordsCount || curLen + words[i].length() + i - lastI > maxWidth)就是处理字符串的判断条件
+ *
  */
 public class No068_Text_Justification {
     public static void main(String[] args) {
         No068_Text_Justification No068 = new No068_Text_Justification();
-        No068.fullJustify(new String[] {
-            "This", "is", "an", "example", "of", "text", "justification."
-        },16);
+        List<String> rst = No068.fullJustify(new String[]{
+                "This", "is", "an", "example", "of", "text", "justification."
+        }, 16);
+        for (String s : rst) {
+            System.out.println(s);
+        }
+
     }
 
     public List<String> fullJustify(String[] words, int maxWidth) {
@@ -45,8 +54,8 @@ public class No068_Text_Justification {
         List<String> rst = new ArrayList<>();
         int curLen = 0;
         int lastI = 0;
-        for (int i = 0; i < wordsCount; i++) {
-            if (i == wordsCount || curLen + words[i].length() - lastI > maxWidth) {
+        for (int i = 0; i <= wordsCount; i++) {
+            if (i == wordsCount || curLen + words[i].length() + i - lastI > maxWidth) {
                 StringBuffer buf = new StringBuffer();
                 int spaceCount = maxWidth - curLen;
                 int spaceSlots = i - lastI - 1;
