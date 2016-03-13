c1aee0840738a0e9e4e4a151899ea40d8a761c2e
Jian Zhang <jianzhang@tcl.com>
2016-03-12 21:55:21 -0800

    finish the coding No105 推导公式比较晦涩，要仔细理解领会

---
+++ src/leetcode/com/medium/No109_Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -0,0 +1,69 @@
+package leetcode.com.medium;
+
+/**
+ * Created by jason on 2016/3/12.
+ * Location:
+ * https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+ * ***********************************************************************
+ * Description:
+ * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ * ***********************************************************************
+ * Solution:
+ * 套用二叉树的判读方法，这样的话总是从最左侧的子节点，子树开始，那么list中的数值也就刚好可以放到结点中
+ */
+public class No109_Convert_Sorted_List_to_Binary_Search_Tree {
+
+    private ListNode curt;
+
+    private int getListLength(ListNode head) {
+        int size = 0;
+
+        while (head != null) {
+            size++;
+            head = head.next;
+        }
+
+        return size;
+    }
+
+    private TreeNode sortedListToBSTHelper(int size) {
+        if (size <= 0) {
+            return null;
+        }
+
+        TreeNode left = sortedListToBSTHelper(size / 2);
+        TreeNode root = new TreeNode(curt.val);
+        curt = curt.next;
+        TreeNode right = sortedListToBSTHelper(size - 1 - size / 2);
+
+        root.left = left;
+        root.right = right;
+        return root;
+
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        curt = head;
+        int size = getListLength(head);
+        return sortedListToBSTHelper(size);
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
