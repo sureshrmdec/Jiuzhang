2c83d2afb806f00ffdc3ae925cd9fe8beee93050
Jian Zhang <jianzhang@tcl.com>
2016-03-13 19:26:29 -0700

    path sum 的递归思路很有意思，return的返回方式很简洁，以后要套用哦

---
+++ src/leetcode/com/util/TreeNode.java
@@ -0,0 +1,15 @@
+package leetcode.com.util;
+
+/**
+ * Created by jason on 2016/3/13.
+ * as a common component
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
