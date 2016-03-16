595bcf74a19da58416138b94f7d9192881430bc4
Jian Zhang <jianzhang@tcl.com>
2016-03-16 09:38:38 -0700

    finish coding

--- src/leetcode/com/easy/No118_Pascals_Triangle.java
+++ src/leetcode/com/easy/No118_Pascals_Triangle.java
@@ -26,28 +26,28 @@ import java.util.List;
 public class No118_Pascals_Triangle {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> rst = new ArrayList<>();
+        List<Integer> prev = new ArrayList<>();
 
-        if (numRows == 0) return rst;
+        if (numRows <= 0) return rst;
 
-        List<Integer> first = new ArrayList<>();
-        first.add(1);
-        rst.add(first);
+        prev.add(1);
+        rst.add(prev);
 
         for (int i = 1; i < numRows; i++) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int j = 0; j < i + 1; j++) {
-                tmp.add(-1);
-            }
+            List<Integer> solution = new ArrayList<>();
 
-            List<Integer> prev = rst.get(i - 1);
-            tmp.set(0, prev.get(0));
-            tmp.set(i, prev.get(i - 1));
+            for (int j = 0; j < i+1; j++) {
+                solution.add(-1);
+            }
+            solution.set(0, prev.get(0));
+            solution.set(solution.size() - 1, prev.get(i-1));
+
             for (int j = 1; j < i; j++) {
-                tmp.set(j, prev.get(j - 1) + prev.get(j));
+                solution.set(j, prev.get(j-1)+prev.get(j));
             }
-            rst.add(tmp);
+            rst.add(solution);
+            prev=solution;
         }
-
         return rst;
     }
 }
