ea4122577a09b7c6f6d7a1876f94b6ae4742e2b5
Jian Zhang <jianzhang@tcl.com>
2016-03-17 10:19:35 -0700

    recover the no115 source

--- src/leetcode/com/hard/No115_Distinct_Subsequences.java
+++ src/leetcode/com/hard/No115_Distinct_Subsequences.java
@@ -1,11 +1,40 @@
 package leetcode.com.hard;
 
 /**
- * Created by jason on 2016/3/17.
- * location:
+ * Created by tcl on 2016/3/16.
+ * Location:
  * https://leetcode.com/problems/distinct-subsequences/
+ * *****************************************************
+ * Description:
+ * Given a string S and a string T, count the number of distinct subsequences of T in S.
+ * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+ * Here is an example:
+ * S = "rabbbit", T = "rabbit"
+ * Return 3.
  * ******************************************************
- *
+ * Solution:
+ * http://www.cnblogs.com/yuzhangcmu/p/4196373.html
  */
 public class No115_Distinct_Subsequences {
+    public int numDistinct(String S, String T) {
+        int rst = 0;
+
+        if (S == null || T == null) return rst;
+
+        int[][] nums = new int[S.length() + 1][T.length() + 1];
+
+        for (int i = 0; i <= S.length(); i++) {
+            nums[i][0] = 1;
+        }
+
+        for (int i = 1; i <= S.length(); i++) {
+            for (int j = 1; j < T.length(); j++) {
+                nums[i][j] = nums[i - 1][j];
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    nums[i][j] += nums[i - 1][j - 1];
+                }
+            }
+        }
+        return nums[S.length()][T.length()];
+    }
 }
