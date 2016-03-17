ddcf9c95734624921bba03994e93888f6372a436
Jian Zhang <jianzhang@tcl.com>
2016-03-16 18:16:45 -0700

    add comments on the differences between offer and add, poll and remove . The former can't throw the exceptions. The latter could throw exception. when the collection is full/empty

--- src/leetcode/com/medium/No117_Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ src/leetcode/com/medium/No117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -36,6 +36,20 @@ public class No117_Populating_Next_Right_Pointers_in_Each_Node_II {
     public void connect(TreeLinkNode root) {
         if (root == null) return;
         Queue<TreeLinkNode> queue = new LinkedList<>();
+        //bug2: don't the differences between offer and add
+        //ref URL:https://docs.oracle.com/javase/7/docs/api/java/util/Queue.html
+        //The offer method inserts an element if possible, otherwise returning false.
+        // This differs from the Collection.add method, which can fail to add an element only
+        // by throwing an unchecked exception. The offer method is designed for use when failure is a normal,
+        // rather than exceptional occurrence, for example, in fixed-capacity (or "bounded") queues.
+//        The remove() and poll() methods remove and return the head of the queue. Exactly which element is removed
+// fro m the queue is a function of the queue's ordering policy, which differs from implementation to implementation.
+
+// The remove() and poll() methods differ only in their behavior when the queue is empty: the remove() method throws
+// an exception, while the poll() method returns null.
+//        The element() and peek() methods return, but do not remove, the head of the queue.
+        //maybe the offer belong to queue interface
+        //
         queue.offer(root);
 
         while (!queue.isEmpty()) {
