5b846bbaa1526cdfc7a8d40100c6fc0e86557293
Jian Zhang <jianzhang@tcl.com>
2016-03-30 22:24:58 -0700

    1.finish coding 2.No179 is not done

---
+++ src/leetcode/com/medium/No179_Largest_Number.java
@@ -0,0 +1,17 @@
+package leetcode.com.medium;
+
+/**
+ * Created by jason on 2016/3/30.
+ * Location:
+ * https://leetcode.com/problems/largest-number/
+ * ************************************************
+ * Description:
+ * Given a list of non negative integers, arrange them such that they form the largest number.
+ * For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+ * Note: The result may be very large, so you need to return a string instead of an integer
+ * ************************************************
+ * Solution:
+ * 完全没有想到解法。看了答案后才明白，用Comparator接口来实现啊，但是要处理下
+ */
+public class No179_Largest_Number {
+}
