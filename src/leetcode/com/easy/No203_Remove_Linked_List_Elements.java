b885bdb6bda6fcdd8417ef9335a61978143aa1e1
TCL Research America <tclresearchamerica@TCLs-MacBook-Pro.local>
2016-04-05 17:33:47 -0700

    test

--- src/leetcode/com/easy/No203_Remove_Linked_List_Elements.java
+++ src/leetcode/com/easy/No203_Remove_Linked_List_Elements.java
@@ -19,7 +19,7 @@ import leetcode.com.util.ListNode;
 public class No203_Remove_Linked_List_Elements {
     public static void main(String[] args) {
         No203_Remove_Linked_List_Elements obj = new No203_Remove_Linked_List_Elements();
-        
+        System.out.print("aaaaa");
     }
 
     public ListNode removeElements(ListNode head, int val) {
