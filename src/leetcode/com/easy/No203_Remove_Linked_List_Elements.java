9eaf0c9a43f6f50e65b006ce25c6ee5ddc044462
TCL Research America <tclresearchamerica@TCLs-MacBook-Pro.local>
2016-04-06 22:17:17 -0700

    finish easy source

--- src/leetcode/com/easy/No203_Remove_Linked_List_Elements.java
+++ src/leetcode/com/easy/No203_Remove_Linked_List_Elements.java
@@ -2,6 +2,9 @@ package leetcode.com.easy;
 
 import leetcode.com.util.ListNode;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 /**
  * Created by tclresearchamerica on 4/5/16.
  * Location:
@@ -20,7 +23,21 @@ public class No203_Remove_Linked_List_Elements {
     public static void main(String[] args) {
         No203_Remove_Linked_List_Elements obj = new No203_Remove_Linked_List_Elements();
         System.out.print("aaaaa");
+        getCurrentUTCTimestamp();
     }
+
+
+    public static String getCurrentUTCTimestamp() {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+//        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+//
+//        final TimeZone utc = TimeZone.getTimeZone("America/Los_Angeles");
+        final TimeZone utc = TimeZone.getTimeZone("PDT");
+        dateFormatter.setTimeZone(utc);
+
+        return dateFormatter.format(new java.util.Date());
+    }
+
 
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(0);
--- src/leetcode/com/easy/No205_Isomorphic_Strings.java
+++ src/leetcode/com/easy/No205_Isomorphic_Strings.java
@@ -2,6 +2,18 @@ package leetcode.com.easy;
 
 /**
  * Created by tclresearchamerica on 4/6/16.
+ * Location:
+ * https://leetcode.com/problems/isomorphic-strings/
+ * ****************************************************
+ * Description:
+ * Given two strings s and t, determine if they are isomorphic.
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character but a character may map to itself.
+ * For example,
+ * Given "egg", "add", return true.
+ * Given "foo", "bar", return false.
+ * Given "paper", "title", return true.
  */
 public class No205_Isomorphic_Strings {
 }
