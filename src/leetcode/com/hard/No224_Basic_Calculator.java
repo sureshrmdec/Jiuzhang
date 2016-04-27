0c39c78ef3b135dc7097dfb0e781fbd6e5458397
TCL Research America <tclresearchamerica@TCLs-MacBook-Pro.local>
2016-04-27 14:46:35 -0700

    学习了取得BST和BT的LCA的算法，找到正确的思路，就可以轻易解决，但是需要进行思路整合，化繁为简

---
+++ src/leetcode/com/hard/No224_Basic_Calculator.java
@@ -0,0 +1,29 @@
+package leetcode.com.hard;
+
+/**
+ * Created by tclresearchamerica on 4/26/16.
+ * Location:
+ * https://leetcode.com/problems/basic-calculator/
+ * ************************************************
+ * Description:
+ * Implement a basic calculator to evaluate a simple expression string.
+ * <p>
+ * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -,
+ * non-negative integers and empty spaces .
+ * <p>
+ * You may assume that the given expression is always valid.
+ * <p>
+ * Some examples:
+ * "1 + 1" = 2
+ * " 2-1 + 2 " = 3
+ * "(1+(4+5+2)-3)+(6+8)" = 23
+ * ************************************************
+ */
+public class No224_Basic_Calculator {
+    public int calculate(String s) {
+        if (s == null) return 0;
+        String temp = s.replaceAll(" ", "");
+        if (temp.length() == 0) return 0;
+
+    }
+}
