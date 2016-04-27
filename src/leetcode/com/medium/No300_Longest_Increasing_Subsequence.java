8b2ff019752c84f41002480b1588472051fafaa7
TCL Research America <tclresearchamerica@TCLs-MacBook-Pro.local>
2016-04-26 17:22:39 -0700

    finish coding,try to finish simple Reverse Polish Notation

---
+++ src/leetcode/com/medium/No300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,21 @@
+package leetcode.com.medium;
+
+/**
+ * Created by tclresearchamerica on 4/26/16.
+ * Location:
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ * ****************************************************************
+ * Description:
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ * <p>
+ * For example,
+ * Given [10, 9, 2, 5, 3, 7, 101, 18],
+ * The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4.
+ * Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+ * <p>
+ * Your algorithm should run in O(n2) complexity.
+ * <p>
+ * Follow up: Could you improve it to O(n log n) time complexity?
+ */
+public class No300_Longest_Increasing_Subsequence {
+}
