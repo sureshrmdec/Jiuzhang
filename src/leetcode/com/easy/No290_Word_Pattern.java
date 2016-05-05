3741ee9b964a958df72b432f0f00efb8d40c7807
jianzhang <vkea95@gmail.com>
2016-05-04 22:22:51 -0700

    finish coding

--- src/leetcode/com/easy/No290_Word_Pattern.java
+++ src/leetcode/com/easy/No290_Word_Pattern.java
@@ -1,5 +1,9 @@
 package leetcode.com.easy;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by tclresearchamerica on 5/4/16.
  * *******************************************
@@ -17,19 +21,33 @@ package leetcode.com.easy;
  * pattern = "aaaa", str = "dog cat cat dog" should return false.
  * pattern = "abba", str = "dog dog dog dog" should return false.
  * *******************************************
- * <p>
+ * bug1:
+ * 向HashMap存储value的时候,没有考虑是否同样的value曾经被放入map
+ * bug2:需要考虑两个比较对象的size是否一致.
  * *******************************************
  */
 public class No290_Word_Pattern {
 
     public boolean wordPattern(String pattern, String str) {
+        HashMap<Character, String> map = new HashMap<>();
+        Set<String> valueSet = new HashSet<String>();
         String[] objArray = str.split(" ");
-        int i = 0;
-        for (i = 0; i < objArray.length && i < pattern.length(); i++) {
-            if (pattern.charAt(i) != objArray[i].charAt(0))
-                return false;
+        if (objArray.length != pattern.length()) return false;
+        for (int i = 0; i < objArray.length && i < pattern.length(); i++) {
+            if (map.containsKey(pattern.charAt(i))) {
+                String value = map.get(pattern.charAt(i));
+                if (!value.equals(objArray[i]))
+                    return false;
+            } else {
+                map.put(pattern.charAt(i), objArray[i]);
+                if (valueSet.contains(objArray[i]))
+                    return false;
+                else
+                    valueSet.add(objArray[i]);
+            }
+
         }
-        return i == objArray.length && i == pattern.length();
+        return true;
 
     }
 }
