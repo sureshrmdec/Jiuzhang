bc29f7154bb49722a0552c00f6b8ad95468dea94
jianzhang <vkea95@gmail.com>
2016-05-15 16:57:19 -0700

    finish coding

---
+++ src/ctc/com/viii/ch1/arrays/No0102_IQ.java
@@ -0,0 +1,20 @@
+package ctc.com.viii.ch1.arrays;
+
+/**
+ * Created by tclresearchamerica on 5/15/16.
+ * **************************************************************
+ * Location:
+ * Pg73@CTC
+ * **************************************************************
+ * Description:
+ * implement a function void reverse(char* str) in C or C++ which reverses a null-terminated string.
+ * **************************************************************
+ * Analysis:
+ * 1.
+ * **************************************************************
+ * No.3 Given two strings, write a method to decide if one is permutation of the other.
+ * **************************************************************
+ * **************************************************************
+ */
+public class No0102_IQ {
+}
