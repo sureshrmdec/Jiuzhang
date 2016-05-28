1fbb4a293441110776310f2b95f1cd26fc1b5136
jianzhang <vkea95@gmail.com>
2016-05-28 10:35:10 -0700

    rewrite No.126

--- src/leetcode/com/hard/No126_Word_Ladder_II.java
+++ src/leetcode/com/hard/No126_Word_Ladder_II.java
@@ -33,42 +33,54 @@ import java.util.*;
  * ***************************************************************
  * Solution:
  * 网络答案:
+ * 使用BFS先构建了一张图,
+ * 然后再用DFS的递归方式构建出各个路径
+ * ***************************************************************
+ * 技术点:
+ * 1.Set的删除方法,可以有removeAll
+ * 2.使用BFS的过程中,根据set的尺寸大小调整,之间的相互关系,进而优化性能
+ * ***************************************************************
+ * Thoughts:
+ * 1.这个题目难度超大,面试中出现的可能性很低,但是BFS,DFS的实践思想,值得借鉴
  * ***************************************************************
  * ***************************************************************
  */
 public class No126_Word_Ladder_II {
 
-    //flag of whether we have connected two parts
+
     boolean isConnected = false;
 
     public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
-        //we use bi-directional BFS to find shortest path
 
-        Set<String> fwd = new HashSet<String>();
+        //we use bi-dirctional BFS to find shortest path
+        Set<String> fwd = new HashSet<>();
         fwd.add(beginWord);
 
-        Set<String> bwd = new HashSet<String>();
+        Set<String> bwd = new HashSet<>();
         bwd.add(endWord);
 
-        Map<String, List<String>> hs = new HashMap<String, List<String>>();
+
+        Map<String, List<String>> hs = new HashMap<>();
         BFS(fwd, bwd, wordList, false, hs);
 
-        List<List<String>> result = new ArrayList<List<String>>();
+        List<List<String>> result = new ArrayList<>();
 
-        //if two parts cannot be connected, then return empty list
+        //if two parts can not be connected, then return empty list
         if (!isConnected) return result;
 
         //we need to add start node to temp list as there is no other node can get start node
-        List<String> temp = new ArrayList<String>();
+        List<String> temp = new ArrayList<>();
         temp.add(beginWord);
 
         DFS(result, temp, beginWord, endWord, hs);
 
         return result;
+
+
     }
 
+    //将预处理的对象，传入BFS方法。
     public void BFS(Set<String> forward, Set<String> backward, Set<String> dict, boolean swap, Map<String, List<String>> hs) {
-
         //boundary check
         if (forward.isEmpty() || backward.isEmpty()) {
             return;
@@ -76,63 +88,68 @@ public class No126_Word_Ladder_II {
 
         //we always do BFS on direction with less nodes
         //here we assume forward set has less nodes, if not, we swap them
+        //这个方法好，动态调整后优化处理效果
         if (forward.size() > backward.size()) {
             BFS(backward, forward, dict, !swap, hs);
+            //bug2:动态调整后，需要直接返回，否则会导致重复的结果出现
             return;
         }
 
-        //remove all forward/backward words from dict to avoid duplicate addition
+        //remove all forward/backward from dict to avoid duplicate addition
+        //new technology:hashmap 可以删除指定的set的数据集
+        //bug1:要删除所有的元素，并不是一个：remove-->removeAll
         dict.removeAll(forward);
         dict.removeAll(backward);
 
         //new set contains all new nodes from forward set
-        Set<String> set3 = new HashSet<String>();
+        Set<String> newSet = new HashSet();
 
         //do BFS on every node of forward direction
         for (String str : forward) {
             //try to change each char of str
             for (int i = 0; i < str.length(); i++) {
                 //try to replace current char with every chars from a to z
-                char[] ary = str.toCharArray();
+                char[] array = str.toCharArray();
                 for (char j = 'a'; j <= 'z'; j++) {
-                    ary[i] = j;
-                    String temp = new String(ary);
+                    array[i] = j;
+                    String temp = new String(array);
 
                     //we skip this string if it is not in dict nor in backward
                     if (!backward.contains(temp) && !dict.contains(temp)) {
                         continue;
                     }
 
+
                     //we follow forward direction
                     String key = !swap ? str : temp;
                     String val = !swap ? temp : str;
 
-                    if (!hs.containsKey(key)) hs.put(key, new ArrayList<String>());
+                    if (!hs.containsKey(key))
+                        hs.put(key, new ArrayList<String>());
 
-                    //if temp string is in backward set, then it will connect two parts
+                    //if temp string in backward set, then it will connect two parts
                     if (backward.contains(temp)) {
                         hs.get(key).add(val);
                         isConnected = true;
                     }
 
-                    //if temp is in dict, then we can add it to set3 as new nodes in next layer
+                    //if temp is in dict, then we can add it to newSet as new nodes in next layer
                     if (!isConnected && dict.contains(temp)) {
                         hs.get(key).add(val);
-                        set3.add(temp);
+                        newSet.add(temp);
                     }
                 }
-
             }
         }
-
-        //to force our path to be shortest, we will not do BFS if we have found shortest path(isConnected = true)
+        //to force our path to be shortest, we will not do BFS if we have found shortest paht(isConnected=true)
         if (!isConnected) {
-            BFS(set3, backward, dict, swap, hs);
+            BFS(newSet, backward, dict, swap, hs);
         }
+
     }
 
     public void DFS(List<List<String>> result, List<String> temp, String start, String end, Map<String, List<String>> hs) {
-        //we will use DFS, more specifically backtracking to build paths
+        //we will use DFS, more specifically backtracking to build paths.
 
         //boundary case
         if (start.equals(end)) {
@@ -140,8 +157,8 @@ public class No126_Word_Ladder_II {
             return;
         }
 
-        //not each node in hs is valid node in shortest path, if we found current node does not have children node,
-        //then it means it is not in shortest path
+        // not each node in hs is valid node in shortest path, if we found current node does not have children node
+        //then it mean it's not in shortest path
         if (!hs.containsKey(start)) {
             return;
         }
@@ -150,7 +167,7 @@ public class No126_Word_Ladder_II {
             temp.add(s);
             DFS(result, temp, s, end, hs);
             temp.remove(temp.size() - 1);
-
         }
+
     }
 }
