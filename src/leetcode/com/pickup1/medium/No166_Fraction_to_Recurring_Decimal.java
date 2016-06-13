a9f4b31377fc0abeb97168c8d16476081c4d1568
jianzhang <vkea95@gmail.com>
2016-06-13 10:17:11 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No166_Fraction_to_Recurring_Decimal.java
@@ -0,0 +1,38 @@
+package leetcode.com.pickup1.medium;
+
+/**
+ * Created by tclresearchamerica on 6/13/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/
+ * ****************************************************
+ * Description:
+ * Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+ * If the fractional part is repeating, enclose the repeating part in parentheses.
+ * For example,
+ * Given numerator = 1, denominator = 2, return "0.5".
+ * Given numerator = 2, denominator = 1, return "2".
+ * Given numerator = 2, denominator = 3, return "0.(6)".
+ * ****************************************************
+ * Thoughts:
+ * 1.数学问题,必然用到数学知识和CS的结合,这里面的问题就是无限循环小数的判断和处理方法,
+ * 单纯的进行除法计算肯定是不可以的,那么是否可以用乘法来进行转换和代替呢?
+ * 可以想到的有余数,除法后的
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No166_Fraction_to_Recurring_Decimal {
+    public static void main(String[] args) {
+        System.out.println(2%3);
+    }
+    public String fractionToDecimal(int numerator, int denominator) {
+        return null;
+    }
+}
