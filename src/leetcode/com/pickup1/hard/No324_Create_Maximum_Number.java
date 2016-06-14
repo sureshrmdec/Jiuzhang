76094c4e6a834e45abdf0a1dff7b1c6495bf1700
jianzhang <vkea95@gmail.com>
2016-06-14 15:18:15 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/hard/No324_Create_Maximum_Number.java
@@ -0,0 +1,45 @@
+package leetcode.com.pickup1.hard;
+
+/**
+ * Created by tclresearchamerica on 6/14/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/create-maximum-number/
+ * ****************************************************
+ * Description:
+ * Given two arrays of length m and n with digits 0-9 representing two numbers. Create the maximum number of
+ * length k <= m + n from digits of the two. The relative order of the digits from the same array must be preserved.
+ * Return an array of the k digits. You should try to optimize your time and space complexity.
+ * <p>
+ * Example 1:
+ * nums1 = [3, 4, 6, 5]
+ * nums2 = [9, 1, 2, 5, 8, 3]
+ * k = 5
+ * return [9, 8, 6, 5, 3]
+ * <p>
+ * Example 2:
+ * nums1 = [6, 7]
+ * nums2 = [6, 0, 4]
+ * k = 5
+ * return [6, 7, 6, 0, 4]
+ * <p>
+ * Example 3:
+ * nums1 = [3, 9]
+ * nums2 = [8, 9]
+ * k = 3
+ * return [9, 8, 9]
+ * <p>
+ * Credits:
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No324_Create_Maximum_Number {
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+
+    }
+}
