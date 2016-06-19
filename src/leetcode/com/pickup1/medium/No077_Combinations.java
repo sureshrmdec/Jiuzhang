02560421233ec6442a843b923f28b2810d8871e5
jianzhang <vkea95@gmail.com>
2016-06-19 15:03:40 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No077_Combinations.java
@@ -0,0 +1,37 @@
+package leetcode.com.pickup1.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tclresearchamerica on 6/17/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/combinations/
+ * ****************************************************
+ * Description:
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * For example,
+ * If n = 4 and k = 2, a solution is:
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No077_Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+}
