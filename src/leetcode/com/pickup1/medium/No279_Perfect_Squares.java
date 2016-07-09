581e46dd09b699eca4ac548418894bd4e3d0d830
jianzhang <vkea95@gmail.com>
2016-07-08 21:59:11 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No279_Perfect_Squares.java
@@ -0,0 +1,32 @@
+package leetcode.com.pickup1.medium;
+
+/**
+ * Created by tclresearchamerica on 7/8/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/perfect-squares/
+ * ****************************************************
+ * Description:
+ * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...)
+ * which sum to n.
+ * For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+ * ****************************************************
+ * Thoughts:
+ * 1.要找到可以平方的数字然后一起搞,因为要找到least的对象,
+ * 所以现在可以想到的有2分法,将目标值除2,再想辙?还是有别的数学逼近方法?
+ * 但是涉及到开放,还真没有什么好招呢啊!再想想啊!
+ * 2.依然是没有想到,看了第一遍的提示,才知道数字可以表示为x=a+b*b
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No279_Perfect_Squares {
+    public int numSquares(int n) {
+        int result = 0;
+
+
+        return result;
+    }
+}
