8992fc4d907b6b75accd29df092b99dac4d9f94e
jianzhang <vkea95@gmail.com>
2016-07-14 09:35:03 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No213_House_Robber_II.java
@@ -0,0 +1,34 @@
+package leetcode.com.pickup1.medium;
+
+/**
+ * Created by tclresearchamerica on 7/14/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/house-robber-ii/
+ * ****************************************************
+ * Description:
+ * After robbing those houses on that street, the thief has found himself a new place for his thievery so that he
+ * will not get too much attention. This time, all houses at this place are arranged in a circle. That means the
+ * first house is the neighbor of the last one. Meanwhile, the security system for these houses remain the same as
+ * for those in the previous street.
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount
+ * of money you can rob tonight without alerting the police.
+ * ****************************************************
+ * Thoughts:
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No213_House_Robber_II {
+    public int rob(int[] nums) {
+        return 0;
+    }
+}
