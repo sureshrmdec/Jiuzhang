2d2ba602a78133033a1b65bcf0820a4a43619d7f
jianzhang <vkea95@gmail.com>
2016-07-18 10:19:27 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No053_Maximum_Subarray.java
@@ -0,0 +1,29 @@
+package leetcode.com.pickup1.medium;
+
+/**
+ * Created by tclresearchamerica on 7/18/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/maximum-subarray/
+ * ****************************************************
+ * Description:
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+ * <p>
+ * For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+ * the contiguous subarray [4,−1,2,1] has the largest sum = 6
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No053_Maximum_Subarray {
+    public int maxSubArray(int[] nums) {
+        int result = 0;
+        return result;
+    }
+}
