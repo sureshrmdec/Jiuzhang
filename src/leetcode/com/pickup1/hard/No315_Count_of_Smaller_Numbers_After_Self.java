b2e0422e6c271ab031d31731660368bfd11fe155
jianzhang <vkea95@gmail.com>
2016-07-18 19:20:05 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/hard/No315_Count_of_Smaller_Numbers_After_Self.java
@@ -0,0 +1,34 @@
+package leetcode.com.pickup1.hard;
+
+import java.util.List;
+
+/**
+ * Created by tclresearchamerica on 7/18/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+ * ****************************************************
+ * Description:
+ * You are given an integer array nums and you have to return a new counts array. The counts array has the property
+ * where counts[i] is the number of smaller elements to the right of nums[i].
+ * Example:
+ * Given nums = [5, 2, 6, 1]
+ * To the right of 5 there are 2 smaller elements (2 and 1).
+ * To the right of 2 there is only 1 smaller element (1).
+ * To the right of 6 there is 1 smaller element (1).
+ * To the right of 1 there is 0 smaller element.
+ * Return the array [2, 1, 1, 0].
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No315_Count_of_Smaller_Numbers_After_Self {
+    public List<Integer> countSmaller(int[] nums) {
+        return null;
+    }
+}
