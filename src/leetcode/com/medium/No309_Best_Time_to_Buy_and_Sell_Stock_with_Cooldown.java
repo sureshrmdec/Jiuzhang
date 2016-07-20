b8ff7c518885b4b33556c6d98dd9e22b119807b6
jianzhang <vkea95@gmail.com>
2016-07-19 22:28:13 -0700

    finish coding

--- src/leetcode/com/medium/No309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ src/leetcode/com/medium/No309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -22,7 +22,14 @@ package leetcode.com.medium;
  * 2.解法是DP吗?还是啥呢?好困啊
  * 3.让我来定义下,最大获利即当前元素的之前元素的最大-最小 然后累计
  * ****************************************************
+ * Reference:   -->还是要继续理解这个说明方法
+ * https://discuss.leetcode.com/topic/30431/easiest-java-solution-with-explanations
+ * Much simpler explanations:
+ * https://discuss.leetcode.com/topic/30680/share-my-dp-solution-by-state-machine-thinking/5
  * ****************************************************
+ * Time: 30 mins
+ * Beat: 50%
+ * Bug:-
  * ****************************************************
  * ****************************************************
  * ****************************************************
@@ -31,7 +38,18 @@ package leetcode.com.medium;
  */
 public class No309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
     public int maxProfit(int[] prices) {
-        int result = 0;
-        return result;
+        if (prices == null || prices.length <= 1) return 0;
+
+        int b0 = -prices[0], b1 = b0;
+        int s0 = 0, s1 = 0, s2 = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            b0 = Math.max(b1, s2 - prices[i]);
+            s0 = Math.max(s1, b1 + prices[i]);
+            b1 = b0;
+            s2 = s1;
+            s1 = s0;
+        }
+        return s0;
     }
 }
---
+++ src/leetcode/com/pickup1/medium/No367_Valid_Perfect_Square.java
@@ -0,0 +1,33 @@
+package leetcode.com.pickup1.medium;
+
+/**
+ * Created by tclresearchamerica on 7/19/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/valid-perfect-square/
+ * ****************************************************
+ * Description:
+ * Given a positive integer num, write a function which returns True if num is a perfect square else False.
+ * Note: Do not use any built-in library function such as sqrt.
+ * Example 1:
+ * Input: 16
+ * Returns: True
+ * Example 2:
+ * Input: 14
+ * Returns: False
+ * ****************************************************
+ * Thoughts:
+ * 1.
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No367_Valid_Perfect_Square {
+    public boolean isPerfectSquare(int num) {
+
+    }
+}
