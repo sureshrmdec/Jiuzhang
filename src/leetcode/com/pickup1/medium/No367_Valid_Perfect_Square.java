0dcfbbee575cc4958e63c41304ace68712dbde72
jianzhang <vkea95@gmail.com>
2016-07-20 10:13:29 -0700

    finish coding

--- src/leetcode/com/pickup1/medium/No367_Valid_Perfect_Square.java
+++ src/leetcode/com/pickup1/medium/No367_Valid_Perfect_Square.java
@@ -17,7 +17,17 @@ package leetcode.com.pickup1.medium;
  * Returns: False
  * ****************************************************
  * Thoughts:
- * 1.
+ * 1. 完全没有想法,这是一道数学问题,有个数学公式,如下
+ * This is a math problem：
+ * 1 = 1
+ * 4 = 1 + 3
+ * 9 = 1 + 3 + 5
+ * 16 = 1 + 3 + 5 + 7
+ * 25 = 1 + 3 + 5 + 7 + 9
+ * 36 = 1 + 3 + 5 + 7 + 9 + 11
+ * ....
+ * so 1+3+...+(2n-1) = (2n-1 + 1)n/2 = nn
+ * 2.参阅之前的答案,发现是通过逼近的方式解决问题,但是为什么这么做,并不知道
  * ****************************************************
  * ****************************************************
  * ****************************************************
@@ -27,7 +37,24 @@ package leetcode.com.pickup1.medium;
  * ****************************************************
  */
 public class No367_Valid_Perfect_Square {
+
     public boolean isPerfectSquare(int num) {
 
+        long r = num;
+        while (r * r > num) {
+            r = (r + num / r) / 2;
+
+        }
+        return r * r == num;
+
+    }
+
+    public boolean isPerfectSquare_standard(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
     }
 }
