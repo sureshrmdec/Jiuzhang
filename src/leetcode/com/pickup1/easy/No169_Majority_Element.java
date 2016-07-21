b58ae6977dd0b95ea23ff727d712ed5fd10b97e2
jianzhang <vkea95@gmail.com>
2016-07-21 08:22:43 -0700

    finish coding

--- src/leetcode/com/pickup1/easy/No169_Majority_Element.java
+++ src/leetcode/com/pickup1/easy/No169_Majority_Element.java
@@ -12,6 +12,11 @@ package leetcode.com.pickup1.easy;
  * <p>
  * You may assume that the array is non-empty and the majority element always exist in the array.
  * ****************************************************
+ * Thought:
+ * 1.Majority Element就是出现次数超过n/2次,
+ * 有一种做法,就是累计计算出现的次数,当然是在hashMap中,
+ * 2.另一种方法,就是不停地比赛删除,最后剩下的就是答案啦,也就是说,我根本不care其余的数字,只是在意这个目标值
+ * 3.参考了第一版的答案,才发现做法比较smart
  * ****************************************************
  * ****************************************************
  * ****************************************************
@@ -20,7 +25,18 @@ package leetcode.com.pickup1.easy;
  */
 public class No169_Majority_Element {
     public int majorityElement(int[] nums) {
-        int result = 0;
-        return result;
+        int[] result = new int[2];
+        for (int num : nums) {
+            if (result[0] == num) {
+                result[1]++;
+            } else if (result[1] == 0) {
+                result[0] = num;
+                result[1]++;
+            } else {
+                result[1]--;
+            }
+//            result -=num==result[0]
+        }
+        return result[0];
     }
 }
---
+++ src/leetcode/com/pickup1/medium/No096_Unique_Binary_Search_Trees.java
@@ -0,0 +1,35 @@
+package leetcode.com.pickup1.medium;
+
+/**
+ * Created by tclresearchamerica on 7/20/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/unique-binary-search-trees/
+ * ****************************************************
+ * Description:
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+ * <p>
+ * For example,
+ * Given n = 3, there are a total of 5 unique BST's.
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ * ****************************************************
+ * Thoughts:
+ * <p>
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No096_Unique_Binary_Search_Trees {
+    public int numTrees(int n) {
+
+    }
+}
