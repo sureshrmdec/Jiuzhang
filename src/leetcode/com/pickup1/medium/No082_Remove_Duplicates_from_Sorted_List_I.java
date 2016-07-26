a195c62898a7b02baa6f6e36a7e3e47fe3c169df
Jian Zhang <jianzhang@tcl.com>
2016-07-25 19:49:05 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No082_Remove_Duplicates_from_Sorted_List_I.java
@@ -0,0 +1,19 @@
+package leetcode.com.pickup1.medium;
+
+import leetcode.com.util.ListNode;
+
+/**
+ * Created by jason on 2016/7/25.
+ */
+public class No082_Remove_Duplicates_from_Sorted_List_I {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next=head;
+        ListNode prev =dummy;
+
+//        for (head!=null){
+//
+//        }
+        return null;
+    }
+}
