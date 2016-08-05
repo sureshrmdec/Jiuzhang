a18608e7f791896d7d1340a21bd271a6b86063f4
jianzhang <vkea95@gmail.com>
2016-08-05 14:42:21 -0700

    No051,052 N-Queens Problem is necessary to understand more

--- src/leetcode/com/medium/No372_Super_Pow.java
+++ src/leetcode/com/medium/No372_Super_Pow.java
@@ -1,4 +1,4 @@
-package leetcode.com.pickup1.medium;
+package leetcode.com.medium;
 
 import java.util.*;
 
@@ -25,7 +25,10 @@ import java.util.*;
  * <p>
  * Result: 1024
  * ****************************************************
+ * 完全不懂
  * ****************************************************
+ * Reference:
+ * https://discuss.leetcode.com/topic/50458/1-liners-other-with-explanations/3
  * ****************************************************
  * ****************************************************
  * ****************************************************
@@ -34,7 +37,26 @@ import java.util.*;
  */
 public class No372_Super_Pow {
     public int superPow(int a, int[] b) {
-        return 0;
+        int res = 1;
+        int p = a;
+        for (int i = b.length - 1; i >= 0; i--) {
+            res = res * pow(p, b[i], 1337) % 1337;
+            p = pow(p, 10, 1337);
+        }
+        return res;
+    }
+
+    public int pow(int a, int b, int c) {
+        long res = 1;
+        long p = a;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                res = (res * p) % c;
+            }
+            p = (p * p) % c;
+            b >>= 1;
+        }
+        return (int) (res % c);
     }
 
 
