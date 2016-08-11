316a229313bc574eee57877dd8c07daa59d41de6
jianzhang <vkea95@gmail.com>
2016-08-10 20:12:40 -0700

    finish coding

--- src/leetcode/com/medium/No309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ src/leetcode/com/medium/No309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -31,13 +31,57 @@ package leetcode.com.medium;
  * Beat: 50%
  * Bug:-
  * ****************************************************
+ * 1. Define States
+ * <p>
+ * To represent the decision at index i:
+ * <p>
+ * buy[i]: Max profit till index i. The series of transaction is ending with a buy.
+ * sell[i]: Max profit till index i. The series of transaction is ending with a sell.
+ * To clarify:
+ * <p>
+ * Till index i, the buy / sell action must happen and must be the last action. It may not happen at index i.
+ * It may happen at i - 1, i - 2, ... 0.
+ * In the end n - 1, return sell[n - 1]. Apparently we cannot finally end up with a buy. In that case,
+ * we would rather take a rest at n - 1.
+ * For special case no transaction at all, classify it as sell[i], so that in the end, we can still return sell[n - 1].
+ * Thanks @alex153 @kennethliaoke @anshu2.
+ * 2. Define Recursion
+ * <p>
+ * buy[i]: To make a decision whether to buy at i, we either take a rest, by just using the old decision at i - 1,
+ * or sell at/before i - 2, then buy at i, We cannot sell at i - 1, then buy at i, because of cooldown.
+ * sell[i]: To make a decision whether to sell at i, we either take a rest, by just using the old decision at i - 1,
+ * or buy at/before i - 1, then sell at i.
+ * So we get the following formula:
+ * <p>
+ * buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
+ * sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+ * 3. Optimize to O(1) Space
+ * <p>
+ * DP solution only depending on i - 1 and i - 2 can be optimized using O(1) space.
+ * <p>
+ * Let b2, b1, b0 represent buy[i - 2], buy[i - 1], buy[i]
+ * Let s2, s1, s0 represent sell[i - 2], sell[i - 1], sell[i]
+ * Then arrays turn into Fibonacci like recursion:
+ * <p>
+ * b0 = Math.max(b1, s2 - prices[i]);
+ * s0 = Math.max(s1, b1 + prices[i]);
+ * 4. Write Code in 5 Minutes
+ * <p>
+ * First we define the initial states at i = 0:
+ * <p>
+ * We can buy. The max profit at i = 0 ending with a buy is -prices[0].
+ * We cannot sell. The max profit at i = 0 ending with a sell is 0.
+ * <p>
  * ****************************************************
+ * buy[i]:截止第i天最大获利,traction的终了动作是买
+ * sell[i]:截止第i天最大获利,traction的终了动作是买
  * ****************************************************
  * ****************************************************
  * ****************************************************
  */
 public class No309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
-    public int maxProfit(int[] prices) {
+
+    public int maxProfit_leetCode(int[] prices) {
         if (prices == null || prices.length <= 1) return 0;
 
         int b0 = -prices[0], b1 = b0;
