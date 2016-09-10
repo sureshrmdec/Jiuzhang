27f8a991d7c3c2bff7c3aed1c208e36c878ee4fe
jianzhang <vkea95@gmail.com>
2016-09-10 14:25:38 -0700

    finish coding

--- src/leetcode/com/pickup1/easy/No234_Palindrome_Linked_List.java
+++ src/leetcode/com/pickup1/easy/No234_Palindrome_Linked_List.java
@@ -21,6 +21,10 @@ import leetcode.com.util.ListNode;
  * ref:https://discuss.leetcode.com/topic/33376/java-easy-to-understand
  * 4.另一个大神的解法,貌似是在寻找中的的过程中,就把链表给逆序了
  * https://discuss.leetcode.com/topic/18293/11-lines-12-with-restore-o-n-time-o-1-space/2
+ * 2016/09/09
+ * 1.和晓波讨论了一下,认为将方法模块化,是比较好的一种方法,此题可拆解为:寻找中点,翻转链表和回文check三个模块
+ * 2.还是要画图解决问题,fast&slow问题,多画几个图就好了
+ *
  * ****************************************************
  * ****************************************************
  * ****************************************************
@@ -31,6 +35,43 @@ import leetcode.com.util.ListNode;
 public class No234_Palindrome_Linked_List {
 
     public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+        ListNode midNode = findMiddleNode(head);
+        midNode = reverse(midNode);
+        return chkPalindrome(head, midNode);
+
+    }
+
+    private ListNode findMiddleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = dummy.next;
+            dummy.next = head;
+            head = next;
+        }
+        return dummy.next;
+    }
+
+    private boolean chkPalindrome(ListNode head, ListNode midNode) {
+        while (midNode != null && midNode.val == head.val) {
+            midNode = midNode.next;
+            head = head.next;
+        }
+        return midNode == null ? true : false;
+    }
+
+    public boolean isPalindrome_1(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
 
@@ -48,7 +89,7 @@ public class No234_Palindrome_Linked_List {
         return slow == null;
     }
 
-    private ListNode reverse(ListNode head) {
+    private ListNode reverse_1(ListNode head) {
         ListNode prev = null;
         while (head != null) {
             ListNode next = head.next;
