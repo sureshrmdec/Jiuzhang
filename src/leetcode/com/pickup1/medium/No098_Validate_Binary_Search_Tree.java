47104459432d3addc0b9b929973c206ed2fdccfe
jianzhang <vkea95@gmail.com>
2016-10-06 08:16:55 -0700

    finish coding

---
+++ src/leetcode/com/pickup1/medium/No098_Validate_Binary_Search_Tree.java
@@ -0,0 +1,44 @@
+package leetcode.com.pickup1.medium;
+
+import leetcode.com.util.TreeNode;
+
+/**
+ * Created by tclresearchamerica on 10/5/16.
+ * ****************************************************
+ * Location:
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ * ****************************************************
+ * Description:
+ * <p>
+ * Assume a BST is defined as follows:
+ * <p>
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * Example 1:
+ * 2
+ * / \
+ * 1   3
+ * Binary tree [2,1,3], return true.
+ * Example 2:
+ * 1
+ * / \
+ * 2   3
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ * ****************************************************
+ */
+public class No098_Validate_Binary_Search_Tree {
+    public void isValidBST(TreeNode root) {
+
+    }
+}
