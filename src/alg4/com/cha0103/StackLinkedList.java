821efd6a585b07e459932ab1f99e400869df287a
jianzhang <vkea95@gmail.com>
2016-12-12 15:01:04 -0800

    finish coding

---
+++ src/alg4/com/cha0103/StackLinkedList.java
@@ -0,0 +1,64 @@
+package alg4.com.cha0103;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * Created by JianZhang on 12/11/16.
+ */
+public class StackLinkedList<Item> implements Iterable<Item> {
+
+    private Node first;
+    private int N;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node currentNode = first;
+
+        @Override
+        public boolean hasNext() {
+            return currentNode != null;//-->not currentNode.next==null
+        }
+
+        @Override
+        public Item next() {
+            Item item = currentNode.item;
+            currentNode = currentNode.next;
+
+            return item;
+        }
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+}
